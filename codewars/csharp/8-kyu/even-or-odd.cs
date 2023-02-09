ec098df69f4cea81e282188d75226272c0965f43
KM Musshafen <kmusshafen@hotmail.com>
2023-02-08 20:44:41 -0500

    Even or Odd

---
+++ codewars/csharp/8-kyu/even-or-odd.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Solution
+{
+    public class SolutionClass
+    {
+        public static string EvenOrOdd(int number)
+        {
+            //if the number modulus is equal to 0 then 
+            if (number % 2 == 0)
+            {
+                //the answer is "Even" 
+                return "Even";
+            }
+
+
+            else
+            {
+                //else the answer is "Odd"
+                return "Odd";
+            }
+        }
+    }
+}
